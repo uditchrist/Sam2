c49ea53ce8c7971b3f63516f09b1229d1c7fa6e3
prabukiwi1 <39737708+prabukiwi1@users.noreply.github.com>
2020-11-28 10:27:35 +0530

    angular

---
+++ app1.js
@@ -0,0 +1,9 @@
+angular.module("MyApp",[])
+.controller("MyCtrl",function($scope)
+{
+    $scope.names=[
+        {name:'Rithul',country:'England',age:22},
+        {name:'Kevin',country:'Denmark',age:26},
+        {name:'Reenu',country:'America',age:29}
+    ];
+});
