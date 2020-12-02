b2551c5dbf57ec15da62a59f84390ef4cf187d71
prabukiwi1 <39737708+prabukiwi1@users.noreply.github.com>
2020-12-02 07:14:00 +0530

    filer_search

--- filter_app.js
+++ filter_app.js
@@ -3,10 +3,11 @@ angular.module("MyApp",[])
 {
     var employees=[
         {name:'Rithul',dob:new Date("July 21,1990"),gender:"Male",salary:55000.778},
-        {name:'Indu',dob:new Date("July 20,1987"),gender:"Feale",salary:65000.778},
+        {name:'Indu',dob:new Date("July 20,1987"),gender:"Female",salary:65000.778},
         {name:'Kevin',dob:new Date("November 19,1983"),gender:"Male",salary:55000.778},
         {name:'Reena',dob:new Date("August 21,1990"),gender:"Male",salary:55000.778}
     ];
+    console.log(employees);
     $scope.employees=employees;
     $scope.rowlimit=4;
 });
